3a891afcf7e41fd94cc222d8cceee34cef4f4c8d
limingsong <2276695595@qq.com>
2025-06-16 11:43:13 +0800

    fix: 优化通知

--- app/service/house_video_clip_service.py
+++ app/service/house_video_clip_service.py
@@ -18,7 +18,7 @@ from app.enum.biz import BizPlatformTaskStatusEnum
 from app.utils.qywx_utils import send_qywx_message
 from app_config import AppConfig, BASE_DIR
 from pyJianYingDraft import trange, Clip_settings
-from app.utils.common_utils import get_current_datetime_str_, download_by_url_to_local
+from app.utils.common_utils import get_current_datetime_str_, download_by_url_to_local, get_current_datetime_str
 from app.utils.oss_utils import init_oss, upload_local_file_to_oss, \
     generate_get_url, process_url
 from pyJianYingDraft.jianying_exporter import JianyingExporter
@@ -286,14 +286,14 @@ def handle_auto_clip_house_video(data: dict) -> JyTaskOutputInfo:
         if not video_script_url:
             raise ValueError("缺少视频脚本地址")
         url = jy_auto_cut_and_export_one_step(task_id=task_id, house_no=house_no, video_script_url=video_script_url)
-        if notify_url:
-            message = (f"AI智能剪辑成功！\n"
-                       f"任务ID:{task_id}\n"
-                       f"房源编号: {house_no}\n"
-                       f"员工号:{staff_id}\n"
-                       f"昵称:{nickname}\n"
-                       f"视频URL: {url}")
-            send_qywx_message(message, url=notify_url)
+        # if notify_url:
+        #     message = (f"AI智能剪辑成功！\n"
+        #                f"任务ID:{task_id}\n"
+        #                f"房源编号: {house_no}\n"
+        #                f"员工号:{staff_id}\n"
+        #                f"昵称:{nickname}\n"
+        #                f"视频URL: {url}")
+        #     send_qywx_message(message, url=notify_url)
         output_info.success = True
         output_info.task_status = BizPlatformTaskStatusEnum.DoneSuccess.value
         output_info.task_message = BizPlatformTaskStatusEnum.DoneSuccess.desc
@@ -302,13 +302,14 @@ def handle_auto_clip_house_video(data: dict) -> JyTaskOutputInfo:
     except Exception as e:
         logger.error("[房源视频自动剪辑] Error: %s", str(e), exc_info=True)
         if notify_url:
-            message = (f"AI智能剪辑失败！\n"
+            message = (f"房源视频AI剪辑失败！\n"
+                       f"当前时间：{get_current_datetime_str()}\n"
                        f"任务ID:{task_id}\n"
                        f"房源编号: {house_no}\n"
                        f"员工号:{staff_id}\n"
                        f"昵称:{nickname}\n"
                        f"错误信息：{str(e)}\n")
-            send_qywx_message(message, url=notify_url)
+            send_qywx_message(url=notify_url, message=message, level='warning')
         output_info.success = False
         output_info.task_status = BizPlatformTaskStatusEnum.DoneFail.value
         output_info.task_message = str(e)
--- app/utils/qywx_utils.py
+++ app/utils/qywx_utils.py
@@ -1,5 +1,5 @@
 """
-@File: qywx_utils.py
+@File: qywx.py
 @Description: 企业微信消息推送
 
 @Author: lms
@@ -10,13 +10,22 @@ import logging
 
 import requests
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s %(levelname)s: %(message)s [in %(filename)s:%(lineno)d]'
-)
+logger = logging.getLogger(__name__)
+
+# 太屋企业微信 星云助手的业务相关 吉吉国王
+TAIWU_WEBHOOK_URL_BUSINESS = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=3fde7210-d944-43d7-8dfb-68613e3d4f6e"
+
+# 太屋企业微信 市场数据视频制作通知 （周末不加班）
+TAIWU_MARKET_VIDEO_NOTICE_URL = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=2c003810-8938-468c-a041-f36ca459a85a'
 
 
-def send_qywx_message(message, url):
+def send_qywx_message(url, message, level="info"):
+    """发送企业微信消息
+    Args:
+        url (str): 企业微信机器人webhook地址
+        message (str): 消息内容
+        level (str): 消息级别，info 或者 warning, 默认为info
+    """
     # 设置请求头
     headers = {
         "Content-Type": "application/json",
@@ -24,24 +33,36 @@ def send_qywx_message(message, url):
         "Host": "qyapi.weixin.qq.com",
         "Connection": "keep-alive"
     }
+
+    # 根据不同的level设置不同的消息内容
+    if level == "info":
+        content = f"<font color=\"info\">**📢新信息看看就行**</font>\n{message}"
+    elif level == "warning":
+        content = f"<font color=\"warning\">**❌出错啦！搞快点！！！**</font>\n{message}"
+    else:
+        content = message
+
     # 设置请求体
     json_payload = {
-        "msgtype": "text",
-        "text": {
-            "content": message
+        "msgtype": "markdown",  # 修改为markdown类型
+        "markdown": {
+            "content": content
         }
     }
     try:
-        # 执行 POST 请求
+        # 执行POST请求
         response = requests.post(url, headers=headers, json=json_payload)
         # 获取响应内容
         response_body = response.text
         logging.info(f"{response_body}")
     except Exception as e:
-        logging.error(f"企业微信消息推送失败 error:{str(e)}")
-        import traceback
-        traceback.print_exc()
+        logging.error(f"企业微信消息推送失败 error:{str(e)}", exc_info=True)
 
 
 if __name__ == '__main__':
-    send_qywx_message("测试消息", "")
+    logging.basicConfig(
+        level=logging.INFO,
+    )
+    url = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=3fde7210-d944-43d7-8dfb-68613e3d4f6e'
+    message = '测试消息'
+    send_qywx_message(url=url, message=message)
